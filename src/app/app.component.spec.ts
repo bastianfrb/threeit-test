bcfd44a5763d2bdfd963526142685822ac197249
Bastian Bahamondez <bastianfrb@gmail.com>
2021-10-29 21:57:30 -0300

    Delete useless code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('threeit');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('threeit app is running!');
-  });
 });
